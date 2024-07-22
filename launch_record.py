705106aba7f88ff9f4257a32ca48c5155a42fb4d
benfromtheforrest <ben@fnconsulting.co.uk>
2024-07-22 12:21:15 +0100

    updated to gpiozero lib

--- launch_record.py
+++ launch_record.py
@@ -1,59 +1,46 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
 import time
 import subprocess
 import os
 import signal
+from gpiozero import Button, LED
 
-# Configuration
 BUTTON_PIN = 12
 LED_PIN = 25
 RUNNING = False
 record_process = None
 
-# Set GPIO mode to GPIO.BOARD
-GPIO.setmode(GPIO.BOARD)
+button = Button(BUTTON_PIN)
+led = LED(LED_PIN)
 
-# Set up the GPIO pins
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-# Define a function to start recording
-def start_recording(channel):
+def start_recording():
     global RUNNING, record_process
     if not RUNNING:
         RUNNING = True
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        record_process = subprocess.Popen(['./new_record.sh'], preexec_fn=os.setsid)
+        led.on()
+        record_process = subprocess.Popen(['/home/crosstech/toughcam-logger/new_record.sh'], preexec_fn=os.setsid)
         print("Recording started")
 
-# Define a function to stop recording
-def stop_recording(channel):
+def stop_recording():
     global RUNNING, record_process
     if RUNNING:
         RUNNING = False
-        GPIO.output(LED_PIN, GPIO.LOW)
+        led.blink(on_time=0.5, off_time=0.5)
         if record_process:
             os.killpg(os.getpgid(record_process.pid), signal.SIGINT)
             record_process.wait()
             print("Recording stopped")
+        led.off()
 
-# Add event detection for the button press
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=start_recording, bouncetime=300)
-GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=stop_recording, bouncetime=300)
+button.when_pressed = start_recording
+button.when_released = stop_recording
 
-# Define a function to handle signals
 def signal_handler(sig, frame):
     if RUNNING:
-        stop_recording(None)
-    GPIO.cleanup()
+        stop_recording()
     exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
 print("Waiting for button press...")
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    time.sleep(1)
