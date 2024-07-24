5cb14e627f322342eb219d3568834aa9d99e66af
benfromtheforrest <ben@fnconsulting.co.uk>
2024-07-24 13:26:08 +0100

    added setup script

--- config/first_time_setup.sh
+++ config/first_time_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-plugins-base \
 sudo apt-get install -y ffmpeg
 
 # Install other necessary packages
-#sudo apt-get install -y python3-pip
+#sudo apt-get install -y python3-pip quota
 
 # Install Python gpiozero library
 pip3 install gpiozero
@@ -25,6 +25,17 @@ mkdir -p /home/crosstech/toughcam-release/output_videos
 chmod +x /home/crosstech/toughcam-release/new_record.sh
 chmod +x /home/crosstech/toughcam-release/upload_service.sh
 chmod +x /home/crosstech/toughcam-release/reset_gps_usb.sh
+
+# Enable quotas on the home directory
+sudo mount -o remount,usrquota,grpquota /home
+
+# Initialize the quota database
+sudo quotacheck -cum /home
+sudo quotaon /home
+
+# Set a quota for the user 'crosstech'
+# Setting 900GB soft and hard limit
+sudo setquota -u crosstech 0 921600000 0 0 /home
 
 # Set up a systemd service to run the main Python script at startup as root
 echo "[Unit]
@@ -42,9 +53,28 @@ User=root
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/toughcam-recording-service.service
 
-# Enable and start the systemd service
+# Set up a systemd service to run the upload Python script at startup as root
+echo "[Unit]
+Description=Run ToughCam Upload Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /home/crosstech/toughcam-release/upload_service.py
+WorkingDirectory=/home/crosstech/toughcam-release
+StandardOutput=syslog
+StandardError=syslog
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/toughcam-upload-service.service
+
+# Enable and start the systemd services
 sudo systemctl enable toughcam-recording-service.service
 sudo systemctl start toughcam-recording-service.service
+
+sudo systemctl enable toughcam-upload-service.service
+sudo systemctl start toughcam-upload-service.service
 
 echo "Installation complete. Rebooting the system..."
 sudo reboot
